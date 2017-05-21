cbc770ce9ef8b83e3c6fd53bbc8e34722c5278fd
Samuel <rott.samuel@gmail.com>
2017-05-21 21:01:57 +0200

    create empty AutoComplete component

---
+++ src/AutoComplete.js
@@ -0,0 +1,22 @@
+import React  from "react";
+
+class AutoComplete extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+
+	render() {
+
+		return (
+
+			<div>
+				hello world!
+			</div>
+
+		);
+	}
+}
+
+
+export default ( AutoComplete );
